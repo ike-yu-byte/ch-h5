6953ab7d49efa064f05ac2523bbbad2c22635c9d
ike_yu <ike_yu@qq.com>
2025-03-04 16:36:01 +0800

    feat: Debit-，封装Axios #201

--- apps/debit/src/api/index.ts
+++ apps/debit/src/api/index.ts
@@ -1,7 +1,34 @@
 import http from '@/utils/request.ts'
+import { static_url, langMap } from '@/config'
+
+function formatData(obj: any) {
+  const lang: any = localStorage.getItem('locale') || 'en'
+  if (lang) {
+    obj.lang = langMap[lang]
+  }
+  obj.token = localStorage.getItem('token') || ''
+  return obj
+}
+
+//获取图片地址
+export const getImageUrl = (image: string) => {
+  const protocal = ['http', 'https']
+  if (!image) return ''
+  if (image && protocal.find((item) => image.includes(item))) {
+    return image
+  } else {
+    return static_url + image
+  }
+}
+
 // 案例API
-export function apiDemo(url: string = '', param: object = {}) {
-  return http.httpRequestGet(url, param)
-  // post方式这么用
-  // http.httpRequestPost(url, param)
+export function apiDemo(param: object = {}) {
+  return http.get('', { params: formatData(param), responseType: 'json' })
+}
+
+// 货币市场行情
+export const getCurrencyMarket = (data: object = {}) => {
+  return http.post('currency/quotation_new', formatData(data), {
+    responseType: 'json',
+  })
 }
--- apps/debit/src/config/index.ts
+++ apps/debit/src/config/index.ts
@@ -59,8 +59,37 @@ export const langList = [
   },
 ]
 
+export const langMap: any = {
+  // 后端接口对应语言传参
+  // 'en': LangEn, // 英语 // en
+  // 	'zh': LangChs, // 繁体中文 zh
+  // 	'hk': LangHk, // 香港中文 zhCN
+  // 	'jp': LangJp, // 日本 ja
+  // 	'th': LangTh, // 泰国 th
+  //     'kr': LangKr, // 韩国 // ko
+  // 	'hi': LangHi, // 印地语 // hi
+  // 	'sp': LangSp, // 西班牙语 // sp
+  en: 'en',
+  zh: 'zh',
+  zhHK: 'hk',
+  ja: 'jp',
+  th: 'th',
+  ko: 'kr',
+  hi: 'hi',
+  sp: 'sp',
+}
+
 export const supportEmail = 'support@debit.com'
 
 export const businessEmail = 'business@debit.com'
 
 export const copyRight = 'Copyright © 2025 Debit Limited. All rights reserved.'
+
+export const baseURL =
+  import.meta.env.MODE === 'development'
+    ? '/api/'
+    : import.meta.env.VITE_BASE_URL || 'http://47.116.15.102:1688/api/'
+
+// 静态资源路径前缀
+export const static_url =
+  import.meta.env.VITE_STATIC_URL || 'http://47.116.15.102:1688/'
